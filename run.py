65c33bb8a1332a148a6a7ccb1ef7db3c503bf5eb
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2019-04-04 18:29:08 +0530

    add dependencies

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ def run_npm():
 	print("WELCOME TO MUSICK")	
 	print("Report Bugs Here: https://github.com/pythongiant/skrrt/issues")
 	w = webview.WebView(width=width, height=width, title="Musick", url="file://"+os.getcwd()+"/index.html", resizable=True, debug=False)
+	
 	w.run()
 def run_flask():
 	os.system("export FLASK_APP=src/finder/finder.py;python3 -m flask run;clear")
